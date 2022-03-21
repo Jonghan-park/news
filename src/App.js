b94178366e57a7b7bd469ff60a7830a1c55c8c07
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-20 19:13:24 -0600

    Create Loading.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Loading from './Loading';
+
+const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=fd933377ec6343f2997122351aaab327'
 
 function App() {
-  return (
-    
-  );
+  const [loading, setLoading] = useState(true);
+  
+  if(loading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
 }
 
 export default App;
