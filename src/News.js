e64f858cd6534cff535ae3d762b62f9789ef9f6b
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-20 21:11:14 -0600

    Update news

--- src/News.js
+++ src/News.js
@@ -7,10 +7,10 @@ const News = ({id, author, title, description, url, urlToImage, removeNews }) =>
         <footer>
             <div className='news-info'>
                 <h4>{title}</h4>
-                <h4>{url}</h4>
+                <a href={url}>Go to original news</a>
             </div>
             <p>
-              {readMore ? description : `${description.substring(0,200)}...`}  
+              {readMore ? description : `${description.substring(0,100)}...`}  
                 <button onClick={() => setReadmore(!readMore)}> 
                 {readMore ? 'show less' : 'Read more'}
                 </button>
